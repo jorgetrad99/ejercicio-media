bbeda74d1f428f224d410b6ace544b8513b6d07e
Jorge Trad <jetradl@gmail.com>
2021-10-16 11:23:29 -0500

    Layout of the app components

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,24 +1,56 @@
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Sample Variance Excercise</h1>
-      </header>  
-      <main>
-        <p>Let's Calculate the sample variance of an array of numbers.</p>
-        <input type="number" />
-        <button>Add</button>
-        <button>Calculate</button>
-        <br />
-        <br />
-        <label>[3, 5, 12, 54, 32,dadajsdashdjasdkajdhakjsdhakjsdhakj djasjd 87]</label>
-        <h2>Calculated sample variance:</h2>
+  const [ array, setArray] = React.useState([]);
 
-      </main>
-      
-    </div>
+  const randomArray = () => {
+    const amountOfNumbers = Math.floor(Math.random() * (30 - 5) + 5);
+    console.log(amountOfNumbers);
+    let provisionalArray = [];
+    for(let i = 0; i < amountOfNumbers; i ++) {
+      provisionalArray.push(Math.floor(Math.random() * 100));
+    }
+    setArray(provisionalArray);
+  }
+
+  const addNumber = () => {
+    const element = document.getElementById("intro");
+    let provisionalArray = array;
+    /* provisionalArray.push */
+  }
+
+  React.useEffect(() => {
+    console.log('Hola');
+  }, [array]);
+
+  return (
+    <React.Fragment>
+      <div className="App">
+        <header className="App-header">
+          <h1>Sample Variance Excercise</h1>
+        </header>  
+        <main>
+          <p>Let's Calculate the sample variance of an array of numbers.</p>
+          <button type="button" onClick={randomArray}>Random Array</button>
+          <div className="container-input">
+            <div>
+              <input type="number" id="add-number-input" placeholder="Type a number..." />
+              <button type="button">Add number</button>
+            </div>
+          </div>
+          <p>Array: {JSON.stringify(array)}</p>
+          <br />
+          <button>Calculate</button>
+          <br />
+          <br />
+          
+          <h2>Calculated sample variance:</h2>
+
+        </main>
+      </div>
+
+    </React.Fragment>
   );
 }
 
