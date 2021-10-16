b517c8a9819321b6fec122ac450b39c4984a3d86
Jorge Trad <jetradl@gmail.com>
2021-10-16 15:32:05 -0500

    Applying the mean formula to obtain the result

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,11 +1,30 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
+import formula from '../formula.svg';
 
 function App() {
   const [ array, setArray] = useState([]);
+  const [ result, setResult ] = useState({
+    sumOfNumbers: null,
+    amountOfNumbers: null
+  })
 
+  const blankResultField = () => {
+    setResult({
+      amountOfNumbers: null,
+      sumOfNumbers: null
+    });
+  }
+
+  const blankInputFiled = () => {
+    var input = document.getElementById("add-number-input");
+    input.value = "";
+  }
 
   const randomArray = () => {
+    if(result.amountOfNumbers !== null && result.sumOfNumbers !== null) {
+      blankResultField();
+    }
     const amountOfNumbers = Math.floor(Math.random() * (30 - 5) + 5);
     console.log(amountOfNumbers);
     let provisionalArray = [];
@@ -16,19 +35,42 @@ function App() {
   }
 
   const addNumber = () => {
+    if(result.amountOfNumbers !== null && result.sumOfNumbers !== null) {
+      blankResultField();
+    }
     const input = Number(document.getElementById("add-number-input").value);
-    console.log(input);
-    setArray( oldArray => [...oldArray, input]);
-    /* let provisionalArray = array;
-    provisionalArray.push(input);
-    console.log(`ArrayB: ${provisionalArray}`);
-    setArray(provisionalArray); */
-    console.log(array);
+    if(input >= 0){
+      setArray( oldArray => [...oldArray, input]);
+    } else {
+      alert('Negative numbers are not allowed');
+    }
+    blankInputFiled();
+  }
+
+  const calculateSampleVariance = () => {
+    if(array.length !== 0) {
+      const sumOfNumbers = array.reduce((preValue = 0, currentValue) => {
+        return preValue + currentValue;
+      })
+      const amountOfNumbers = array.length;
+  
+      console.log(`Suma: ${sumOfNumbers}`);
+      console.log(`Cantidad de numeros: ${amountOfNumbers}`);
+
+      setResult({
+        amountOfNumbers: amountOfNumbers,
+        sumOfNumbers: sumOfNumbers
+      });
+    } else {
+      alert('Insert an element into the array');
+    }
+    /* console.log(result.amountOfNumbers + result.sumOfNumbers); */
+    
   }
 
   useEffect(() => {
     console.log(array);
-  }, [array]);
+  }, [array, result]);
 
   return (
     <React.Fragment>
@@ -41,18 +83,41 @@ function App() {
           <button type="button" onClick={randomArray}>Random Array</button>
           <div className="container-input">
             <div>
-              <input type="number" id="add-number-input" placeholder="Type a number..." />
+              <input type="number" min="0" minLength="1" pattern="[0-9]*" id="add-number-input" placeholder="Type a number..." required />
               <button type="button" onClick={addNumber}>Add number</button>
             </div>
           </div>
           <p>Array:</p>
           <br />
           <p className="showing-array">{JSON.stringify(array)}</p>
+          {/*  */}
           
-          <button>Calculate</button>
-          <br />
+          { array.length !== 0 && 
+              <button type="button" onClick={calculateSampleVariance}>Calculate</button>
+          }
           
-          <h2>Calculated sample variance:</h2>
+          
+          
+          { (result.amountOfNumbers !== null && result.sumOfNumbers !== null) &&
+            <React.Fragment>
+              <h3>Applying the mean formula to obtain the result</h3>
+              <br />
+              <img src={formula} width="320rem" alt="Formula of the sample variance" />
+              <br />
+              <br />
+              <p>Sum of the Terms: {result.sumOfNumbers}</p>
+              <div id="container-line">
+                <hr width="250rem" />
+              </div>
+              <p>Number of the Terms: {result.amountOfNumbers}</p>
+              <br />
+              <h3>Calculated sample variance:</h3>
+              <p>{result.sumOfNumbers / result.amountOfNumbers}</p>
+              <br />
+            </React.Fragment>
+          }
+          
+          
 
         </main>
       </div>
